08f61fdbc523a171fbe63dae61b388312b026dcd
Jack Agtual <jaagtual@gmail.com>
2022-09-17 08:07:08 -0700

    Making gird elements take up entire height of row

--- app.js
+++ app.js
@@ -1,28 +1,26 @@
 const gridSize = 3;
 const grid = document.querySelector('.grid');
-const h1 = document.createElement('h1');
-h1.innerText = 'Testing';
 
-// grid.appendChild(h1);
-
+// loop through rows
 for (let row = 0; row < gridSize; row++) {
     const child = document.createElement('div');
     
-    child.style['flex-grow'] = 1;
-    child.style['align-items'] = 'center';
-    child.style['justify-content'] = 'space-between';
     child.style['display'] = 'flex';
+    child.style['flex-grow'] = 1;
+    child.style['justify-content'] = 'space-between';
+    child.style['align-items'] = 'center';
 
+    // loop through columns
     for (let col = 0; col < gridSize; col++) {
         const subChild = document.createElement('div');
         subChild.classList.add('grid-el')
-        subChild.innerText = `Element ${row}, ${col}`;
-        subChild.style['text-align'] = 'center';
 
         subChild.style['flex-grow'] = 1;
+        subChild.style['height'] = '100%'
+        subChild.style['text-align'] = 'center';
+
         child.appendChild(subChild);
     }
-    
 
     grid.appendChild(child);
 }
